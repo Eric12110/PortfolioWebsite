c8508a319a8b0987d62e08e340e1e9cc4c214971
Eric <yes850520004@gmail.com>
2025-11-06 15:09:02 +0800

    新增render網域

--- MyBackend/wwwroot/js/config.js
+++ MyBackend/wwwroot/js/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = window.location.hostname.includes("web.app")
-    ? "http://127.0.0.1:5101"   // 本機後端
-    : "http://localhost:5101";
+// 決定要連線的 API 伺服器位置
+const API_BASE_URL = window.location.hostname.includes("localhost") 
+    ? "http://localhost:5101" // 本地測試
+    : "https://portfoliowebsite-b45w.onrender.com"; // Render 正式站
